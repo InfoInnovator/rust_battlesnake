85bd9791b3f4d41f0af62d234235a4112a178467
Malte <malte.sparenborg@protonmail.com>
2025-04-15 22:59:27 +0200

    add docs

--- battlesnakes/src/lib.rs
+++ battlesnakes/src/lib.rs
@@ -8,14 +8,27 @@ use snakes::{battlerat::BattleratFactory, carlo_constrictor::CarloConstrictorFac
 pub mod game;
 pub mod snakes;
 
+/// A trait to be implemented by all Battlesnake factories.
+/// This trait is used to create a new Battlesnake instance and provide
+/// information about the snake.
 pub trait BattlesnakeFactory {
+    /// Get the name of the snake.
     fn name(&self) -> String;
+
+    /// Return the /info route for the specific snake.
+    ///
+    /// This does not work right now, because the new Battlesnake API
+    /// does not provide the snake name with the info request.
     fn info(&self) -> Value;
+
+    /// Get a move from the snake.
     fn handle_move(&self, game: &Game, turn: &i32, board: &Board, you: &Battlesnake) -> Move;
 }
 
+/// A trait for the `BattlesnakeFactory` to be used by the rocket server.
 pub type BoxedBattlesnakeFactory = Box<dyn BattlesnakeFactory + Send + Sync>;
 
+/// A function to create a vector of all available factories.
 #[must_use]
 pub fn add_all_factories() -> Vec<BoxedBattlesnakeFactory> {
     vec![
